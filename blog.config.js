f67dc58a3a66bb8a52d857ca4f053cf9b14adf4f
milaimx <152942840+milaimx@users.noreply.github.com>
2023-12-06 21:04:32 -0600

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Milai', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'El mejor directorio de proveedores mayoristas del centro de la CDMX', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.oferta.cc', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Plaza Mayoreo,Los mejores precios del centro,La plaza de los chinos,El mejor directorio de proveedores mayoristas,distribuidores especializados y fabricantes,Directorio de importadores chinos', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Plaza Mayoreo', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
